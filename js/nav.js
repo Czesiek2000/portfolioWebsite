b5b98079db8408e17fdf275dadf3c9a4b62c3f4a
Czesiek2000 <michal.czech00@gmail.com>
2021-10-03 23:54:59 +0200

    Rewrite navbar functionality in pure js

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,17 @@
-$(document).ready(function(){
-    const menu = $('.menu');
-    const nav = $('nav ul');
-    const header = $('header');
-    const logo = $('.logo');
-    $(menu).click(function () {
-        $(nav).toggleClass('active');
-        $(header).toggleClass('opening');
-        $(logo).toggleClass('disable');
-    });
+const menu = document.querySelector('.menu');
+const nav = document.getElementById('main-nav')
+const header = document.querySelector('.header');
+const logo = document.querySelector('.logo');
 
+menu.addEventListener('click', () => {
+    header.classList.toggle('opening');
+    if (logo.style.display === 'none') {
+        logo.style.display = 'flex';
+        menu.style.paddingTop = '0';
+    }else {
+        logo.style.display = 'none';
+        menu.style.paddingTop = '60px';
+    }
 
-});
+    nav.classList.toggle('hide')
+})
