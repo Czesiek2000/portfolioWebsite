f006d602b390354fa09a6991d74e34e13946bf83
Czesiek2000 <michal.czech00@gmail.com>
2021-10-07 01:09:56 +0200

    Rewrite scroll functions without jquery

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,16 @@
-const srollTop = document.querySelector('.scrollTop');
-$(".scrollTop").on("click", function() {
-    $("html").animate({scrollTop: 0}, "slow");
+// scroll to specific link after click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
 });
+
+// scroll top
+document.querySelector('.scrollTop').addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth'})
+})
